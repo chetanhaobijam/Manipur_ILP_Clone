9ac0cce8beb9b3fce41fde4cfcbdc89eb55b950f
Haobijam Chetan Singh <chetanhaobijam@gmail.com>
2021-02-05 16:49:29 +0530

    Made changes to About Section

---
+++ assets/scripts/script.js
@@ -0,0 +1,13 @@
+const jump = document.querySelector(".jump");
+const body = document.querySelector("body");
+
+
+// const scrollTop = () => {
+//   if(document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+//     jump.style.display = "block";
+//   } else {
+//     jump.style.display = "none";
+//   }
+// }
+
+scrollTop();
