cbd5a83f1289ae303cf09982e101c20ba29e0719
Haobijam Chetan Singh <chetanhaobijam@gmail.com>
2021-04-24 16:46:53 +0530

    Adding Animations

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -23,3 +23,52 @@ document.addEventListener("scroll", handleScroll);
 // End of Scroll To Top Button Action
 
 // Start of Permit Sub-grid Action
+const zoomIconOne = document.querySelector(".zoomIconOne");
+const zoomGridOne = document.querySelector(".zoomGridOne");
+const zoomIconTwo = document.querySelector(".zoomIconTwo");
+const zoomGridTwo = document.querySelector(".zoomGridTwo");
+const zoomIconThree = document.querySelector(".zoomIconThree");
+const zoomGridThree = document.querySelector(".zoomGridThree");
+const zoomIconFour = document.querySelector(".zoomIconFour");
+const zoomGridFour = document.querySelector(".zoomGridFour");
+
+const zoomInOne = () => {
+  zoomIconOne.classList.add("zoom");
+}
+
+const zoomOutOne = () => {
+  zoomIconOne.classList.remove("zoom");
+}
+
+const zoomInTwo = () => {
+  zoomIconTwo.classList.add("zoom");
+}
+
+const zoomOutTwo = () => {
+  zoomIconTwo.classList.remove("zoom");
+}
+
+const zoomInThree = () => {
+  zoomIconThree.classList.add("zoom");
+}
+
+const zoomOutThree = () => {
+  zoomIconThree.classList.remove("zoom");
+}
+
+const zoomInFour = () => {
+  zoomIconFour.classList.add("zoom");
+}
+
+const zoomOutFour = () => {
+  zoomIconFour.classList.remove("zoom");
+}
+
+zoomGridOne.addEventListener("mouseenter", zoomInOne);
+zoomGridOne.addEventListener("mouseleave", zoomOutOne);
+zoomGridTwo.addEventListener("mouseenter", zoomInTwo);
+zoomGridTwo.addEventListener("mouseleave", zoomOutTwo);
+zoomGridThree.addEventListener("mouseenter", zoomInThree);
+zoomGridThree.addEventListener("mouseleave", zoomOutThree);
+zoomGridFour.addEventListener("mouseenter", zoomInFour);
+zoomGridFour.addEventListener("mouseleave", zoomOutFour);
